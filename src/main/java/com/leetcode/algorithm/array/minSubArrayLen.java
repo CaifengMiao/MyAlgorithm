3c32b8dd6d84d6da8ab07c585d473890b6b2ce1b
Caifeng.Miao <18516147952@163.com>
2021-07-15 23:34:46 +0800

    minSubArrayLen sliding window algorithm

---
+++ src/main/java/com/leetcode/algorithm/array/minSubArrayLen.java
@@ -0,0 +1,55 @@
+package com.leetcode.algorithm.array;
+
+/**
+ * Given an array of positive integers nums and a positive integer target,return the minimal length of a contiguous
+ * subarray [numsl, numsl+1, ..., numsr-1, numsr] of which the sum is greater than or equal to target.
+ * If there is no such subarray, return 0 instead.
+ *
+ * Example 1:
+ * Input: target = 7, nums = [2,3,1,2,4,3] , Output: 2
+ * Explanation: The subarray [4,3] has the minimal length under the problem constraint.
+ *
+ * Example 2:
+ * Input: target = 4, nums = [1,4,4] , Output: 1
+ *
+ * Example 3:
+ * Input: target = 11, nums = [1,1,1,1,1,1,1,1] , Output: 0
+ *
+ * from：LeetCode
+ * Link：https://leetcode-cn.com/problems/minimum-size-subarray-sum
+ */
+public class minSubArrayLen {
+    /**
+     * This is a very classic sliding window algorithm.
+     * define left and right tow pointers ，first move the right pointer until the sum of the subarray composed of two
+     * pointers is greater than or equal to the target value, and then moves the left pointer to reduce the size of the
+     * window, until that the sum of the subarray is less than the target value, and then moves the right pointer...
+     * Loop the above operation we can get the minimum array length satisfying the condition
+     * @param target
+     * @param nums
+     * @return
+     */
+    public static int minSubArrayLen(int target, int[] nums) {
+        int minLen = 0, subSum = 0, left = 0, right = 0;
+
+        while(right<nums.length && left<nums.length){
+            while(subSum < target && right<nums.length) {
+                subSum = subSum + nums[right];
+                right++;
+                if (subSum >= target) {
+                    minLen = minLen == 0 ? right - left:Math.min(minLen,right-left);
+                }
+            }
+            while(subSum>=target && left<nums.length){
+                subSum = subSum - nums[left];
+                left++;
+                if (subSum >= target) {
+                    minLen = Math.min(minLen,right-left);
+                }
+            }
+        }
+
+        return minLen;
+    }
+
+}
