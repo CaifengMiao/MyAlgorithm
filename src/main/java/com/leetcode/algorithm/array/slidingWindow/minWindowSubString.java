09a711e710f88396eda9da4dfcc5d61f889897e1
Caifeng.Miao <18516147952@163.com>
2021-07-20 23:57:04 +0800

    minSubString

---
+++ src/main/java/com/leetcode/algorithm/array/slidingWindow/minWindowSubString.java
@@ -0,0 +1,66 @@
+package com.leetcode.algorithm.array.slidingWindow;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Given two strings s and t of lengths m and n respectively, return the minimum window substring of s such that every
+ * character in t (including duplicates) is included in the window.
+ * If there is no such substring, return the empty string "".
+ *
+ * The testcases will be generated such that the answer is unique.
+ *
+ * A substring is a contiguous sequence of characters within the string.
+ *
+ * Example 1:
+ * Input: s = "ADOBECODEBANC", t = "ABC"
+ * Output: "BANC"
+ * Explanation: The minimum window substring "BANC" includes 'A', 'B', and 'C' from string t.
+ *
+ * Example 2:
+ * Input: s = "a", t = "a"
+ * Output: "a"
+ * Explanation: The entire string s is the minimum window.
+ * Example 3:
+ *
+ * Input: s = "a", t = "aa"
+ * Output: ""
+ * Explanation: Both 'a's from t must be included in the window.
+ * Since the largest window of s only has one 'a', return empty string.
+ *
+ * From：力扣（LeetCode）
+ * Link：https://leetcode-cn.com/problems/minimum-window-substring
+ */
+public class minWindowSubString {
+    public static String minWindow(String s, String t) {
+        String minSubString="";
+        int[] map = new int[128];
+        //record the number of each character in t
+        for (char ch : t.toCharArray())
+            map[ch]++;
+        //total characters number of t
+        int count = t.length();
+        int left = 0, right = 0, windowLength = Integer.MAX_VALUE;
+        int minSubStart = 0;
+        while (right < s.length()) {
+            if(map[s.charAt(right++)]-->0){
+                count--;
+            }
+            //record smaller windows
+            while (count == 0) {
+                if (right - left < windowLength) {
+                    windowLength = right - left;
+                    minSubStart = left;
+                }
+                if (map[s.charAt(left++)]++ == 0) {
+                    count++;
+                }
+            }
+        }
+        if (windowLength != Integer.MAX_VALUE)
+            minSubString = s.substring(minSubStart, minSubStart + windowLength);
+        return minSubString;
+    }
+    
+}
